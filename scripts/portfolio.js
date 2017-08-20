8b885ceba13835fc41f7152befb4e9a92eaf058d
Kathryn Bettuzzi <katb2z123@gmail.com>
2017-08-20 02:14:16 -0500

    Version 2.0

---
+++ scripts/portfolio.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+$(".hide-on-click").click(function() {
+  $(".hide-on-click").hide();
+});
+
+
+
+
+});
+$(".show-on-click").click(function(){
+      $(".show-on-click").show(img class="panoramaB" src="img/brun1.JPG" alt="Panoramic Photo of Schonbrunn" style="width: 50%; height: 50%;")>;
+  });
